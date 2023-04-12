15b8ef8b2a6d9364658bf6738c14d7b11a0b5cfc
unknown <i015373@aia-apps.biz>
2023-04-12 11:44:24 +0700

    add GetFilePaths function

---
+++ file.go
@@ -0,0 +1,33 @@
+package pipeline
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"path/filepath"
+)
+
+func GetFilePaths(ctx context.Context, folder string, errChannel chan<- error) <-chan string {
+	outChannel := make(chan string)
+
+	go func() {
+		defer close(outChannel)
+
+		errChannel <- filepath.Walk(folder, func(path string, info fs.FileInfo, err error) error {
+
+			if !info.Mode().IsRegular() {
+				return nil
+			}
+
+			select {
+			case <-ctx.Done():
+				return fmt.Errorf("Walk Canceled")
+			case outChannel <- path:
+			}
+			return nil
+		})
+
+	}()
+
+	return outChannel
+}
--- file_test.go
+++ file_test.go
@@ -5,12 +5,11 @@ import (
 	"testing"
 )
 
-func TestGetFilePaths(t *testing.T) {
+func TestGetPaths(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	errChannel := make(chan error)
-
-	defer cancel()
 	defer close(errChannel)
+	defer cancel()
 
 	filePaths := GetFilePaths(ctx, "samples", errChannel)
 	EndDebug(ctx, cancel, filePaths, errChannel)
